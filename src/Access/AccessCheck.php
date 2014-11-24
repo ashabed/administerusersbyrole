787edec9c2d7a894955b0f886a254352ea4df36c
Ashraf Abed <ashrafabed@gmail.com>
2014-11-24 04:37:37 -0500

    Create, edit, and cancel user permissions now work.

--- src/Access/AccessCheck.php
+++ src/Access/AccessCheck.php
@@ -20,22 +20,31 @@ class AccessCheck implements AccessCheckInterface {
   public function applies(Route $route) {
     if ($route->getpath() == '/user/{user}/edit')
       return TRUE;
-//      return TRUE;
+    if ($route->getpath() == '/user/{user}/cancel')
+      return TRUE;
+    if ($route->getpath() == '/admin/people/create')
+      return TRUE;
     return FALSE;
-//    return '_access_yourmodule_page';
   }
 
-
   public function access(AccountInterface $account, Route $route) {
-    dpm($account->id(),'user account id');
     $other_user = \Drupal::request()->attributes->get('user');
-    dpm($other_user->id(), 'user object id');
-
-    return AccessResult::allowedIf(_administerusersbyrole_can_edit_user($account, $other_user));
-
-
-//      $account, 'edit users with no custom roles');
-    return AccessResult::allowedIfHasPermission($account, 'edit users with no custom roles');
+    $path = $route->getPath();
+    if ($path == '/user/{user}/edit') {
+      $uid = $other_user->id();
+      $roles = $other_user->getRoles();
+      $can_edit = _administerusersbyrole_can_edit_user($uid, $roles);
+      return AccessResult::allowedIf($can_edit);
+    }
+    if ($path == '/user/{user}/cancel') {
+      $uid = $other_user->id();
+      $roles = $other_user->getRoles();
+      $can_cancel = _administerusersbyrole_can_cancel_user($uid, $roles);
+      return AccessResult::allowedIf($can_cancel);
+    }
+    if ($path == '/admin/people/create') {
+      return AccessResult::allowedIfHasPermission($account, 'create users');
+    }
   }
 }
 ?>
