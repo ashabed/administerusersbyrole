c2b9137ababa7b72f8504d0b73135bfb639665ea
Ashraf Abed <ashrafabed@gmail.com>
2014-11-24 00:56:13 -0500

    Use AccessCheck to update access rules

--- src/Access/AccessCheck.php
+++ src/Access/AccessCheck.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @file
- * Contains \Drupal\administerusersbyrole\Access\CustomAccessCheck.
+ * Contains \Drupal\administerusersbyrole\Access\AccessCheck.
  */
 
-namespace Drupal\administerusersbyrole;
+namespace Drupal\administerusersbyrole\Access;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Access\AccessCheckInterface;
@@ -15,21 +15,18 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * Checks access for displaying configuration translation page.
  */
-class AdministerusersbyroleAccessCheck implements AccessCheckInterface {
+class AccessCheck implements AccessCheckInterface {
 
-  /**
-   * Checks access.
-   *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The currently logged in account.
-   *
-   * @return \Drupal\Core\Access\AccessResultInterface
-   *   The access result.
-   */
-  public function access(AccountInterface $account) {
-    // Check permissions and combine that with any custom access checking needed. Pass forward
-    // parameters from the route and/or request as needed.
-    return $account->hasPermission('do example things') && $this->someOtherCustomCondition();
+  public function applies(Route $route) {
+    return FALSE;
+//    return '_access_yourmodule_page';
+  }
+
+
+  public function access(Route $route, Request $request, AccountInterface $account) {
+    // TODO: ACCESS
+    //return static::DENY;
+    return AccessResult::allowedIfHasPermission($account, 'Edit users with no custom roles');
   }
 }
 ?>
